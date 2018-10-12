8e672eeec8935d48a13a79cc9a419d7de1744ad7
Юлия Чащина <uliacasina@macbook-pro-ulia.local>
2018-10-12 11:21:54 +0300

    add a shuffle() method to shuffle the cards

--- MemoryGame/MemoryGame.swift
+++ MemoryGame/MemoryGame.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+
 class MemoryGame {
     
    private(set) var cards = [Card]()
@@ -65,8 +66,33 @@ class MemoryGame {
 //            cards.append(card)
 //            cards.append(card)
             cards += [card, card]
+           
         }
-        // TODO: Shuffle the cards
+        // Shuffle the cards
+        cards.shuffle()
     }
-    
+}
+
+extension MutableCollection {
+    /// Shuffles the contents of this collection.
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+        
+        for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            // Change `Int` in the next line to `IndexDistance` in < Swift 4.1
+            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let i = index(firstUnshuffled, offsetBy: d)
+            swapAt(firstUnshuffled, i)
+        }
+    }
+}
+
+extension Sequence {
+    /// Returns an array with the contents of this sequence, shuffled.
+    func shuffled() -> [Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
 }
